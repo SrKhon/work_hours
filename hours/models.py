6d84fef8fedbd6766d5dc8010c82784a03bd66b0
Sergei <cer-chon@yandex.ru>
2022-06-09 22:25:14 +0900

    edit model.py and index.html

--- hours/models.py
+++ hours/models.py
@@ -11,13 +11,15 @@ class Shift(models.Model):
 
 
 class WorkDays(models.Model):
-    date = models.DateField('Дата', default=timezone.now, auto_now=False, null=True)
+    date = models.DateField('기본금', default=timezone.now, auto_now=False, null=True)
     slug = models.SlugField(max_length=50, unique=True)
     shift = models.ForeignKey(Shift, on_delete=models.SET_NULL, null=True, related_name='work_days', verbose_name='Смена')
     day = models.IntegerField(default=0, verbose_name='Кибон')
     night = models.IntegerField(default=0, verbose_name='Ночные')
     overtime = models.IntegerField(default=0, verbose_name='Чаноб')
-    holyday_work = models.IntegerField(default=0, verbose_name='Тыкын')
+    holiday_pay = models.IntegerField(default=0, verbose_name='Тыкын')
+    holiday_overtime_pay = models.IntegerField(default=0, verbose_name='Тыкын-чаноб')
+    parking_allowance = models.IntegerField(default=0, verbose_name='Парковка')
 
     def __str__(self):
         return str(self.date)
