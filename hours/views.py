6dea0e03f226b0044b2d00aa7bfe731583d1a31c
serg <cer-chon@yandex.ru>
2022-06-12 06:36:22 +0900

    Counting hours for the Day field

--- hours/views.py
+++ hours/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
+from django.db.models import *
 from .models import WorkDays, Month
 
 
 def days(request):
     work_days = WorkDays.objects.all()
     months = Month.objects.all()
-    return render(request, 'hours/index.html', {'work_days': work_days, 'months': months})
+    hours_month = WorkDays.objects.aggregate(Day=Sum('day'))
+    return render(request, 'hours/index.html', {'work_days': work_days, 'months': months, 'hours_month': hours_month['Day']})
 
 
 def tb_month(request, slug):
     months = Month.objects.all()
-    month = Month.objects.get(slug__iexact=slug)
-    return render(request, 'hours/get_month.html', {'month': month, 'months': months})
+    month = WorkDays.objects.filter(month__slug=slug)
+    sum_hours = WorkDays.objects.filter(month__slug=slug).aggregate(Day=Sum('day'))
+    return render(request, 'hours/get_month.html', {'month': month, 'months': months, 'sum_hours': sum_hours['Day']})
 
 
 
