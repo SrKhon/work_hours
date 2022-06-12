2b54e7c7c98a4ad50cf1d20dcfc6ca1b04dd65ef
SrKhon <102689123+SrKhon@users.noreply.github.com>
2022-06-12 09:15:04 +0900

    Update views.py

--- hours/views.py
+++ hours/views.py
@@ -7,16 +7,14 @@ def days(request):
     work_days = WorkDays.objects.all()
     months = Month.objects.all()
     hours_month = WorkDays.objects.aggregate(Day=Sum('day'))
-    context = {'work_days': work_days, 'months': months, 'hours_month': hours_month['Day']}
-    return render(request, 'hours/index.html', context)
+    return render(request, 'hours/index.html', {'work_days': work_days, 'months': months, 'hours_month': hours_month['Day']})
 
 
 def tb_month(request, slug):
     months = Month.objects.all()
     month = WorkDays.objects.filter(month__slug=slug)
     sum_hours = WorkDays.objects.filter(month__slug=slug).aggregate(Day=Sum('day'))
-    context = {'month': month, 'months': months, 'sum_hours': sum_hours['Day']}
-    return render(request, 'hours/get_month.html', context)
+    return render(request, 'hours/get_month.html', {'month': month, 'months': months, 'sum_hours': sum_hours['Day']})
 
 
 
