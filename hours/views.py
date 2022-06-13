0cbc7b2a1da02db2c6370f2d29524cc88d5b44f7
serg <cer-chon@yandex.ru>
2022-06-14 05:18:27 +0900

    changed quryset to display current month in views.py

--- hours/views.py
+++ hours/views.py
@@ -26,9 +26,8 @@ def days(request):
 
 
 def tbl_month(request, slug):
-    months = Month.objects.all()  # To list the months
-    month = WorkDays.objects.filter(month__slug=slug)  # To display class fields
-    current_month = Month.objects.get(slug__iexact=slug)  # To display current Month
+    months = Month.objects.all()  # To list the months in menu navbar
+    hours = WorkDays.objects.filter(month__slug=slug)  # To display class fields end current month
     sum_hours_day = WorkDays.objects.filter(month__slug=slug).aggregate(Day=Sum('day'))
     sum_hours_night = WorkDays.objects.filter(month__slug=slug).aggregate(Night=Sum('night'))
     sum_hours_overtime = WorkDays.objects.filter(month__slug=slug).aggregate(Overtime=Sum('overtime'))
@@ -36,7 +35,7 @@ def tbl_month(request, slug):
     sum_hours_holiday_overtime_pay = WorkDays.objects.filter(month__slug=slug).aggregate(Holiday_overtime_pay=Sum('holiday_overtime_pay'))
     sum_hours_parking_allowance = WorkDays.objects.filter(month__slug=slug).aggregate(Parking_allowance=Sum('parking_allowance'))
     context = {
-        'month': month,
+        'hours': hours,
         'months': months,
         'sum_hours_day': sum_hours_day['Day'],
         'sum_hours_night': sum_hours_night['Night'],
@@ -44,6 +43,5 @@ def tbl_month(request, slug):
         'sum_hours_holiday_pay': sum_hours_holiday_pay['Holiday_pay'],
         'sum_hours_holiday_overtime_pay': sum_hours_holiday_overtime_pay['Holiday_overtime_pay'],
         'parking_allowance': sum_hours_parking_allowance['Parking_allowance'],
-        'current_month': current_month,
     }
     return render(request, 'hours/get_month.html', context)
